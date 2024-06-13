ce991ee4ff8a14f1823f068b552c6a1dedca1433
luis felipel <lffteclado@gmail.com>
2024-06-13 11:20:56 -0300

    Atualização das Imagens

---
+++ src/components/CardCategoria.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import type ICategoria from '@/interfaces/ICategoria';
+import type { PropType } from 'vue';
+
+export default {
+    props: {
+        categoria: {type: Object as PropType<ICategoria>, required: true}
+    }
+}
+</script>
+<template>
+<article class="categoria">
+    <header class="categoria_cabecalho">
+        <img src="" alt="" class="categoria_imagem">
+    </header>
+</article>
+</template>
