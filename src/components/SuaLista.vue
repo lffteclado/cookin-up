0de0f15fbfae94b0d8c881cf5a18680e17a71f6b
luis felipel <lffteclado@gmail.com>
2024-06-13 14:08:15 -0300

    Ajustes Componente SuaLista

---
+++ src/components/SuaLista.vue
@@ -0,0 +1,56 @@
+<script lang="ts">
+import Tag from './Tag.vue';
+import type { PropType } from 'vue';
+export default {
+    props: {
+        ingredientes: { type: Array as PropType<string[]>, required: true }
+    },
+
+    components: { Tag }
+}
+</script>
+<template>
+    <section>
+        <span class="subtitulo-lg sua-lista-texto">
+            Sua lista:
+        </span>
+
+        <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
+            <li v-for="ingrediente in ingredientes" :key="ingrediente">
+                <Tag :texto="ingrediente" ativa />
+            </li>
+        </ul>
+
+        <p v-else class="paragrafo lista-vazia">
+            <img src="../assets/imagens/icones/lista-vazia.svg" alt="Ícone de pesquisa">
+            Sua lista está vazia, selecione ingredientes para iniciar.
+        </p>
+    </section>
+</template>
+<style scoped>
+.ingredientes-sua-lista {
+    display: flex;
+    justify-content: center;
+    gap: 1rem 1.5rem;
+    flex-wrap: wrap;
+}
+
+.sua-lista-texto {
+  color: var(--coral, #F0633C);
+  display: block;
+  text-align: center;
+  margin-bottom: 1.5rem;
+}
+
+
+.lista-vazia {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 0.25rem;
+
+  color: var(--coral, #F0633C);
+  text-align: center;
+}
+</style>
